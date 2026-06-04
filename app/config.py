62688e86cff53e00f063a013ed179d7e0f359dbc
Ivan Kolokoltsev <ikolokoltsev@gmail.com>
2026-06-04 22:18:31 +0200

    Fixed model name, and expaneded gitignore

--- app/config.py
+++ app/config.py
@@ -1,7 +1,7 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
-    model_name: str = "smollm2:135m"
+    model_name: str = "HuggingFaceTB/SmolLM2-135M-Instruct"
     max_file_size_mb: int = 10
     
     model_config = SettingsConfigDict(env_file=".env")
